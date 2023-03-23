0b9ba014023da4659df1c3ae9ad7d502c0b0c20d
dhl7799 <donghyundhl@naver.com>
2023-03-23 15:14:00 +0900

    emaillist-postresql

--- emaillist/backend/src/main/java/com/douzone/emaillist/controller/ApiController.java
+++ emaillist/backend/src/main/java/com/douzone/emaillist/controller/ApiController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,10 +43,9 @@ public class ApiController {
 				.body(JsonResult.success(emaillistVo));
 	}
 	
-	@GetMapping("/delete")
-	public ResponseEntity<JsonResult> deleteEmail(String no) {
-		long long_no = Long.parseLong(no);
-		emaillistRepository.delete(long_no);
+	@DeleteMapping("/delete/{no}")
+	public ResponseEntity<JsonResult> deleteEmail(@PathVariable("no") Long no) {
+		emaillistRepository.delete(no);
 
 		return ResponseEntity
 				.status(HttpStatus.OK)
